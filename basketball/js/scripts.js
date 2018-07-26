cfb72b980ac5d18d986bb8a14391ce7cd562c136
stellar1227 <halfjin7@gmail.com>
2018-07-26 18:54:46 +0900

    Object를 활용한 refactoring

--- basketball/js/scripts.js
+++ basketball/js/scripts.js
@@ -25,17 +25,24 @@
    * 5. 10점 이상 이기고 있을 경우는 30, 23으로 다운
    */
 
-    //변수 선언
-    var comScore = 0;
-    var comPercent2 = 0.5;
-    var comPercent3 = 0.33;
-
-    var userScore = 0;
-    var userPercent2 = 0.5;
-    var userPercent3 = 0.33;
-
-    var isComputerTurn = true;
-    var shotsLeft = 15;
+    //변수 선언 - Object로 연관된 변수 처리
+    //컴퓨터
+    var computer = {
+        score : 0,
+        percent2 : 0.5,
+        percent3 : 0.33
+    }
+    //사용자
+    var user = {
+        score : 0,
+        percent2 : 0.5,
+        percent3 : 0.33
+    }
+    //게임관련
+    var game = {
+        isComputerTurn : true,
+        shotsLeft : 15
+    }
     
     //안내멘트 출력
     function showText(str){
@@ -46,15 +53,15 @@
     //컴퓨터 점수 갱신
     function updateComputerScore(score){
         var comScoreElem = document.getElementById('computerScore');
-        comScore += score;
-        comScoreElem.innerHTML = comScore;
+        computer.score += score;
+        comScoreElem.innerHTML = computer.score;
     }
 
     //사용자 점수 갱신
     function updateUserScore(score){
         var userScoreElem = document.getElementById('userScore');
-        userScore += score;
-        userScoreElem.innerHTML = userScore;
+        user.score += score;
+        userScoreElem.innerHTML = user.score;
     }
 
     //컴퓨터 버튼 비 활성화
@@ -73,70 +80,48 @@
         }
     }
 
-    //컴퓨터 슛 이벤트
+    //컴퓨터 슛 이벤트 
     function onComputerShoot(){
-        if(!isComputerTurn) return;
+        if(!game.isComputerTurn) return;
         var shootType = Math.random() < 0.5 ? 2: 3;
+        //if문 리팩토링
 
-        if(shootType === 2){
-            if(Math.random() < comPercent2){
-                //2점슛 1/2확률로 성공
-                showText('컴퓨터가 2점슛을 성공 시켰습니다');
-                updateComputerScore(2);
-            }else{
-                //실패
-                showText('컴퓨터가 2점슛을 실패했습니다.');
-            }
+        if(Math.random() < computer['percent' + shootType]){
+            showText('컴퓨터가' + shootType + '점 슛을 성공시켰습니다.');
+            updateComputerScore(shootType);
         }else{
-            if(Math.random() < comPercent3){
-                //3점 슛 1/3 확률로 성공
-                showText('컴퓨터가 3점슛을 성공 시켰습니다');
-                updateComputerScore(3);
-            }else{
-                //실패
-                showText('컴퓨터가 2점슛을 실패했습니다.');
-            }
+            showText('컴퓨터가' + shootType + '점슛을 실패했습니다.');
         }
-        isComputerTurn = false;
+        
+        game.isComputerTurn = false;
         disableComputerButtons(true);
         disableUserButtons(false);
     }
 
     //유져 슛 이벤트
     function onUserShoot(shootType){
-        if(isComputerTurn) return;
-        if(shootType === 2){
-            if(Math.random() < userPercent2){
-                //2점슛 1/2확률로 성공
-                showText('사용자가 2점슛을 성공 시켰습니다');
-                updateUserScore(2)
-            }else{
-                //실패
-                showText('사용자가 2점슛을 실패했습니다.');
-            }
+        if(game.isComputerTurn) return;
+        //if문 리팩토링
+
+        if(Math.random() < computer['percent' + shootType]){
+            showText('사용자가' + shootType + '점 슛을 성공시켰습니다.');
+            updateComputerScore(shootType);
         }else{
-            if(Math.random() < userPercent3){
-                //3점 슛 1/3 확률로 성공
-                showText('사용자가 3점슛을 성공 시켰습니다');
-                updateUserScore(3);
-            }else{
-                //실패
-                showText('사용자가 2점슛을 실패했습니다.');
-            }
+            showText('사용자가' + shootType + '점슛을 실패했습니다.');
         }
 
-        isComputerTurn = true;
+        game.isComputerTurn = true;
         disableComputerButtons(false);
         disableUserButtons(true);
 
-        shotsLeft--;
+        game.shotsLeft--;
         var shotsLeftElem = document.getElementById('reaminShoot');
-        shotsLeftElem.innerHTML = shotsLeft;
+        shotsLeftElem.innerHTML = game.shotsLeft;
 
-        if(shotsLeft === 0){
-            if(userScore > comScore){
+        if(game.shotsLeft === 0){
+            if(user.score > computer.score){
                 showText('승리했습니다');
-            }else if(userScore === comScore){
+            }else if(user.score === computer.score){
                 showText('비겼습니다');
             }else{
                 showText('졌습니다.');
@@ -151,23 +136,23 @@
 
     //AI 개선 
     function updateAI(){
-        var diff = userScore - comScore;
+        var diff = user.score - computer.score;
 
         if(diff >= 10){
             console.log('확률10증가')
-            comPercent2 = 0.6;
-            comPercent3 = 0.38;
+            computer.percent2 = 0.6;
+            computer.percent3 = 0.38;
         }else if( diff >= 6){
             console.log('확률6증가')
-            comPercent2 = 0.7;
-            comPercent3 = 0.38;
+            computer.percent2 = 0.7;
+            computer.percent3 = 0.38;
         }else if( diff <= -10){
             console.log('확률10감소')
-            comPercent2 = 0.4;
-            comPercent3 = 0.28;
+            computer.percent2 = 0.4;
+            computer.percent3 = 0.28;
         }else if(diff <= -6){
             console.log('확률6감소')
-            comPercent2 = 0.3;
-            comPercent3 = 0.23;
+            computer.percent2 = 0.3;
+            computer.percent3 = 0.23;
         }
     }
